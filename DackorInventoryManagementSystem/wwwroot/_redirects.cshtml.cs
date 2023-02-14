e5c452e5fb670f3c6e6b63eddd5c889fad46a15e
MattDaigle93 <matttdaigle@gmail.com>
2023-02-14 08:09:32 -0500

    Added _redirect

---
+++ DackorInventoryManagementSystem/wwwroot/_redirects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DackorInventoryManagementSystem.wwwroot
+{
+    public class _redirectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
